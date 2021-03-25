64e30b06e14bd84eca991d5fe5d3e6022eff9407
Anchit Gupta <anchit19060@iiitd.ac.in>
2021-03-25 12:30:48 +0530

    Added IG links

--- README.md
+++ README.md
@@ -14,17 +14,4 @@ Here are some ideas to get you started:
 - 😄 Pronouns: AchiWolf
 - ⚡ Fun fact: I lived in 7 states throughout my life in India.
 
-
-<div class="col-md-10" style="text-align: center;">
-    <ul class="social list-inline">
-        <li class="list-inline-item"><a href="https://github.com/anchitgupt" target="_blank" rel="noopener"><i
-                    class="fa fa-github-square fa-3x" title="GitHub"></i></a></li>
-        <li class="list-inline-item"><a href="https://www.linkedin.com/in/anchitgupt" target="_blank"
-                rel="noopener"><i class="fa fa-linkedin-square fa-3x" title="LinkedIn"></i></a></li>
-        <li class="list-inline-item"><a href="https://twitter.com/Anchit_Gupt" target="_blank" rel="noopener"><i
-                    class="fa fa-twitter-square fa-3x" title="Twitter"></i></a></li>
-    </ul>
-</div>
-
----
 Updated at: Thursday, March 25, 11:35 AM GMT+5:30.
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // index.js
 const Mustache = require('mustache');
 const fs = require('fs');
+const puppeteerService = require('./services/puppeteer.service');
 const MUSTACHE_MAIN_DIR = './main.mustache';
 /**
   * DATA is the object that contains all
@@ -19,17 +20,43 @@ let DATA = {
     timeZone: 'Asia/Kolkata',
   }),
 };
-/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
-function generateReadMe() {
+
+async function setInstagramPosts() {
+  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('visitstockholm', 3);
+  DATA.img1 = instagramImages[0];
+  DATA.img2 = instagramImages[1];
+  DATA.img3 = instagramImages[2];
+}
+
+
+async function generateReadMe() {
   fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
   });
 }
-// console.log(DATA.name, DATA.date)
-generateReadMe();
+
+async function action() {
+  /**
+   * Fetch Weather
+   */
+  // await setWeatherInformation();
+
+  /**
+   * Get pictures
+   */
+  await setInstagramPosts();
+
+  /**
+   * Generate README
+   */
+  await generateReadMe();
+
+  /**
+   * Fermeture de la boutique 👋
+   */
+  await puppeteerService.close();
+}
+
+action();
