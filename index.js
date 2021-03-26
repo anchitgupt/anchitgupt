e0fd34932f53ebcab3ccbf548da52442bdca857d
Anchit Gupta <anchit19060@iiitd.ac.in>
2021-03-26 10:22:35 +0530

    Fixing the nodemodules

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // index.js
+
 const Mustache = require('mustache');
 const fs = require('fs');
 const puppeteerService = require('./services/puppeteer.service');
+
+
 const MUSTACHE_MAIN_DIR = './main.mustache';
 /**
   * DATA is the object that contains all
@@ -21,6 +24,10 @@ let DATA = {
   }),
 };
 
+/**
+* Function to get the Latest Instagram Posts from the Account.   
+*
+*/
 async function setInstagramPosts() {
   const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('incredibleindia', 3);
   DATA.img1 = instagramImages[0];
@@ -28,6 +35,10 @@ async function setInstagramPosts() {
   DATA.img3 = instagramImages[2];
 }
 
+
+/**
+* Function to send the DATA into the README
+*/
 
 async function generateReadMe() {
   fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
